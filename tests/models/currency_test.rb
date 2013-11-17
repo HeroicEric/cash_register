04dfb10ed09bf2d393fd94f77dde3174be1fbef3
Eric Kelly <heroiceric@gmail.com>
2013-11-17 18:05:27 -0500

    Small refactor

--- tests/models/currency_test.rb
+++ tests/models/currency_test.rb
@@ -1,9 +1,6 @@
 require_relative '../test_helper'
 
 describe Currency do
-  let(:valid_amounts) { [3, 4.9, 3.50, 0.47, "3.50", "3"] }
-  let(:invalid_amounts) { ['abc', '0.343', 0.4933] }
-
   describe ".format" do
     it "formats the amount to USD" do
       Currency.format(3).must_equal "$3.00"
@@ -11,6 +8,9 @@ describe Currency do
   end
 
   describe ".is_valid_amount?" do
+    let(:valid_amounts) { [3, 4.9, 3.50, 0.47, "3.50", "3"] }
+    let(:invalid_amounts) { ['abc', '0.343', 0.4933] }
+
     it "returns true when amount is valid" do
       valid_amounts.each do |amount|
         Currency.is_valid_amount?(amount).must_equal true
